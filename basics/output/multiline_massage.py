04ce634905a229fbefab3974738d558a248981a4
rcharle <92854092+rcharle@users.noreply.github.com>
2021-10-28 12:27:12 +0000

    Updated code to display a longer multi-line massage

--- basics/output/multiline_massage.py
+++ basics/output/multiline_massage.py
@@ -1,2 +1,9 @@
 print ("system failure imminent!")
 print ("system reboot has been initiated!")
+
+
+print ("System failure imminent!")
+print ("System reboot has been initiated")
+print("...rebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine")
